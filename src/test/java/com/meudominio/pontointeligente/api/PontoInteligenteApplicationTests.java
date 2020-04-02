5eab8cf63149a99fcde5b9fce208e6b2981cd107
marceloplis <marceloplis@yahoo.com.br>
2020-04-02 19:37:50 -0300

    Commit inicial

---
+++ src/test/java/com/meudominio/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
