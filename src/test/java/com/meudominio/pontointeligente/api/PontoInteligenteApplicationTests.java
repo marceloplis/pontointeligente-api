5449a98f7916c605056d0e36e1084afd0474b6ab
marceloplis <marceloplis@yahoo.com.br>
2020-04-04 18:45:26 -0300

    Add Repositories, Services e Tests

--- src/test/java/com/meudominio/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/meudominio/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -2,8 +2,10 @@ package com.meudominio.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PontoInteligenteApplicationTests {
 
 	@Test
